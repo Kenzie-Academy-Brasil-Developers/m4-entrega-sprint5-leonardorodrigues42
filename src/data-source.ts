481e15f93804a6e803d3c4c141db2f5db0826724
leonardorodrigues42 <leonardorodriguesaai@outlook.com>
2022-10-28 17:14:02 -0300

    feat: subindo projeto

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,15 +11,13 @@ const AppDataSource = new DataSource(
     } :
     {
         type: "postgres",
-        host: process.env.HOST,
-        port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
+        url: process.env.DATABASE_URL,
+        ssl: process.env.NODE_ENV === "production"
+            ? {rejectUnauthorized: false} : false,
         logging: true,
         synchronize: true,
-        entities: ['src/entities/*.ts'],
-        migrations: ['src/migrations/*.ts']
+        entities: process.env.NODE_ENV === "production" ? ['dist/src/entities/*.js'] : ['src/entities/*.ts'],
+        migrations: process.env.NODE_ENV === "production" ? ['dist/src/migrations/*.js'] : ['src/migrations/*.ts']
     }
 )
 
